6d744fcd5285f54818c5b340a8168b921f7587d7
tayutaedomo <tayutaedomo@gmail.com>
2018-11-14 10:03:23 +0900

    Fix packaging config

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var packager = require('electron-packager');
 
 gulp.task('package:darwin', function(done) {
   packager({
-    dir: 'app',               // Directory of Application
+    dir: '.',                 // Directory of Application
     out: 'release/darwin',    // Directory of .app or .exe
     name: 'Electron',
     arch: 'x64',              // x64 or ia32
     platform: 'darwin',       // darwin or win32 or linux
-    version: '1.7.9',         // Electron version
+    version: '3.0.9',         // Electron version
     overwrite: true
   }, function (err, path) {
     done();
